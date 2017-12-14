a60087e1d562c337857a4565868d0c55fe0327c7
Melissa Orgill <orgillmelissa33@gmail.com>
2017-12-14 13:12:18 -0800

    displaying one order

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -55,7 +55,7 @@ class App extends React.Component{
 						}
 					</ul>
 				</div>
-				<Order/>
+				<Order fishes={this.state.fishes} order={this.state.order}/>
 				<Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
 			</div>
 		)
