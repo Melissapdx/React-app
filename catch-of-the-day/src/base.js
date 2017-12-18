5f042d2131e6433bd4081021d15c38657ca09105
Melissa Orgill <orgillmelissa33@gmail.com>
2017-12-18 12:46:07 -0800

    added firebase for State

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,11 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass( {
+  // Initialize Firebase
+    apiKey: "AIzaSyDwKhTmfuN-kh2n3fW9DNvenUKgNUr1vyQ",
+    authDomain: "catch-of-the-day-melissa.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-melissa.firebaseio.com",
+  
+  });
+
+export default base;
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,7 @@ import Order from './Order';
 import Inventory from './Inventory';
 import Fish from './Fish';
 import sampleFishes from '../sample-fishes.js'
+import base from '../base';
 
 
 class App extends React.Component{
@@ -19,6 +20,18 @@ class App extends React.Component{
 			order: {}
 		};
 	}
+
+	componentWillMount() {
+		this.ref = base.syncState(`${this.props.params.storeId}/fishes`
+			, {
+				context: this,
+				state: 'fishes'
+			})
+	}
+	componentWillUnmount() {
+		base.removeBinding(this.ref);
+	}
+
 	addFish(fish) {
 		//update state
 		const fishes = {...this.state.fishes};
